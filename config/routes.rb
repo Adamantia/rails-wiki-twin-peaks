0418243c8a4fe42495b102b8ca026d278d9b068c
Mandy T <adamantia.t@gmail.com>
2018-06-05 15:37:26 +0200

    twinpeakquotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'articles#index'
+  resources :articles
+  patch 'articles/:id/content', to: 'tasks#content', as: :content
+
 end
